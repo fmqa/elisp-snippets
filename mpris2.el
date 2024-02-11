5c2d98f3cf36138c3c55424dfc9a31939734ccc4
Ф.З. Мкауед <7354509+fmqa@users.noreply.github.com>
2024-02-11 17:04:59 +0100

    Update mpris2.el
    Untabify

--- mpris2.el
+++ mpris2.el
@@ -6,17 +6,17 @@
 
 (defun my/mpris2-property-changed (svc props _)
   (when-let ((metadata (alist-get "Metadata" props nil nil #'equal))
-			 (metadata-1 (caar metadata)))
-	(setq my/*mpris2-metadata* metadata-1)))
+             (metadata-1 (caar metadata)))
+    (setq my/*mpris2-metadata* metadata-1)))
 
 (defun my/mpris2-current-track ()
   "Returns the name of the last active MPRIS2 track"
   (let* ((metadata my/*mpris2-metadata*)
-		 (xesam-artist (alist-get "xesam:artist" metadata nil nil #'equal))
-		 (xesam-title (alist-get "xesam:title" metadata nil nil #'equal)))
-	(format "%s - %s"
-			(string-join (flatten-list xesam-artist) ", ")
-			(string-join (flatten-list xesam-title)))))
+         (xesam-artist (alist-get "xesam:artist" metadata nil nil #'equal))
+         (xesam-title (alist-get "xesam:title" metadata nil nil #'equal)))
+    (format "%s - %s"
+            (string-join (flatten-list xesam-artist) ", ")
+            (string-join (flatten-list xesam-title)))))
 
 (defun mpris2-yank-current-track ()
   "Inserts the name of the last active MPRIS2 track at point"
@@ -30,10 +30,10 @@
 
 ;; Register handler for media events
 (setq my/*mpris2-registration*
-	  (dbus-register-signal
-	   :session
-	   nil
-	   "/org/mpris/MediaPlayer2"
-	   "org.freedesktop.DBus.Properties"
-	   "PropertiesChanged"
-	   #'my/mpris2-property-changed))
+      (dbus-register-signal
+       :session
+       nil
+       "/org/mpris/MediaPlayer2"
+       "org.freedesktop.DBus.Properties"
+       "PropertiesChanged"
+       #'my/mpris2-property-changed))
